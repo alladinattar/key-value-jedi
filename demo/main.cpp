947928d71458693e3b866eada133b6465808c2de
rinat <rinamuka4@gmail.com>
2021-04-21 13:55:46 -0700

    start coding

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,33 @@
 #include <header.hpp>
+#include "iostream"
+#include <boost/program_options.hpp>
+#include "header.hpp"
+namespace po = boost::program_options;
 
-int main() {
+int main(int argc, char* argv[]) {
+  po::options_description desc("Usage:\n  dbcs [options] <path/to/input/storage.db>\nMust have options");
+  desc.add_options()
+      ("help", "produce help message")
+      ("log-level", po::value<std::string>(), "info|warning|error \ndefault: error")
+      ("thread-count", po::value<int>(), "default: count of logical core")
+      ("output ",po::value<std::string>(), "<path/to/output/storage.db>\ndefault: <path/to/input/dbcs-storage.db>");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+
+
+
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+    return 1;
+  }
+
+
+
+
+
+
 
 }
--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,10 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2021 Rinat Mukaev <rinamuka4@gmail.com>
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+#include "rocksdb/db.h"
+#include <assert.h>
+
+
 
 #endif // INCLUDE_HEADER_HPP_
